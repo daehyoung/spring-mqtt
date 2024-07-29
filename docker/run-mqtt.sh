845d92d8bd4ffef283221c0d33a8d29bf9764f9f
조대형 <daehyoung.cho@gmail.com>
2024-07-29 12:01:12 +0900

    mosquitto설정 수정

--- docker/run-mqtt.sh
+++ docker/run-mqtt.sh
@@ -1,6 +1,8 @@
 docker run -d  \
+  --restart unless-stopped \
   --name mqtt \
   -p 1883:1883 \
   -p 9001:9001 \
   -v $(pwd)/mosquitto.conf:/mosquitto/config/mosquitto.conf \
+  -v $(pwd)/mosquitto_passwd:/mosquitto/config/mosquitto_passwd \
   eclipse-mosquitto
